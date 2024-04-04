dd08e338f74dad35def2b1b11c1ba392646268b9
kolyo <you@example.com>
2024-04-04 19:07:07 +0300

    Created ExchangeApplication with spring initializr

---
+++ exchange-app/src/test/java/com/kolyo/exchange/app/ExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolyo.exchange.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
